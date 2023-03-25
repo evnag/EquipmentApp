a0f65343048e714927eff6242e7d8d2ef22629d1
evnag <vskonev22@gmail.com>
2023-03-25 19:18:13 +0300

    Initial commit

---
+++ src/main/java/com/example/equipmentapp/EquipmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.equipmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/equipmentapp/EquipmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.equipmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
