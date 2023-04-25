d0f808f38a45abb464789e91c8ecd2fa912f62b9
evnag <vskonev22@gmail.com>
2023-04-25 18:31:05 +0300

    feat: add abstract FormEvent

---
+++ src/main/java/com/example/equipmentapp/backend/event/FormEvent.java
@@ -0,0 +1,36 @@
+package com.example.equipmentapp.backend.event;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.formlayout.FormLayout;
+
+public abstract class FormEvent extends ComponentEvent<FormLayout> {
+
+    private final Object object;
+
+    public FormEvent(FormLayout source, Object object) {
+        super(source, false);
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public static class SaveEvent extends FormEvent {
+        public SaveEvent(FormLayout source, Object object) {
+            super(source, object);
+        }
+    }
+
+    public static class DeleteEvent extends FormEvent {
+        public DeleteEvent(FormLayout source, Object object) {
+            super(source, object);
+        }
+    }
+
+    public static class CloseEvent extends FormEvent {
+        public CloseEvent(FormLayout source) {
+            super(source, null);
+        }
+    }
+}
